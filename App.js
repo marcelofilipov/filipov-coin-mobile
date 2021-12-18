cc40edbc6d837f563de58c7db26cfca3116f494d
Marcelo Filipov <marcelo.filipov@gmail.com>
2021-12-18 08:52:08 -0300

    Initial implementation

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, StatusBar, Text, SafeAreaView, Platform } from 'react-native';
+
+import CurrentPrice from './src/components/CurrentPrice';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+        backgroundColor='#f50d41'
+        barStyle='dark-content'
+      />
+      <CurrentPrice/>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? 40 : 0
   },
 });
